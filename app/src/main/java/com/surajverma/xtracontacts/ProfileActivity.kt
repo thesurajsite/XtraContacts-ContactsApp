e7edc58216d0a239cb2e82b50a9e95003baf0296
Suraj Verma <thesurajsite@gmail.com>
2024-12-08 15:18:07 +0530

    Added Vibrator

--- app/src/main/java/com/surajverma/xtracontacts/ProfileActivity.kt
+++ app/src/main/java/com/surajverma/xtracontacts/ProfileActivity.kt
@@ -29,7 +29,9 @@ class ProfileActivity : AppCompatActivity() {
         binding.logoutButton.setOnClickListener {
             vibrator.vibrate(50)
             auth.signOut()
-            val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
             startActivity(intent)
             finish()
         }
