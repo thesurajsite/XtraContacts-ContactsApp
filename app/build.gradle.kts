064e7842f14c436bb1aa34c56fc29a414d5376f4
Suraj Verma <thesurajsite@gmail.com>
2024-12-18 11:45:25 +0530

    Google Signin Done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.surajverma.xtracontacts"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
